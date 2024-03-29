094404484c7bac579bd7a962021cf11e37d67710
sskhi1 <sskhi20@freeuni.edu.ge>
2024-03-30 01:13:27 +0400

    added postgresql as datasource

---
+++ auth-service/src/main/java/com/freeuni/macs/authservice/model/db/User.java
@@ -0,0 +1,33 @@
+package com.freeuni.macs.authservice.model.db;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users", schema = "macscode_schema", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_generator")
+    @SequenceGenerator(name = "user_generator", sequenceName = "user_id_seq", allocationSize = 1, schema = "macscode_schema")
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    @Email
+    private String email;
+
+    private String hashedPassword;
+}
