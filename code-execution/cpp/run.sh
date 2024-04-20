be61f4895e6ca3f920b77d2b7a085525c970331f
aadua20 <aadua20@freeuni.edu.ge>
2024-04-20 22:53:51 +0400

    add time and memory limit

--- code-execution/cpp/run.sh
+++ code-execution/cpp/run.sh
@@ -4,21 +4,27 @@
 cd execution
 
 mkdir result
+
 echo ${test_count} > result/test_count.txt
 
 g++ src/main.cpp -o src/main -std=c++11
 
-echo ${time_limit} > result/test_count.txt
+ulimit -t ${time_limit}
 
-echo ${time_limit}
-
-#ulimit -t ${time_limit}
+ulimit -v ${memory_limit}
 
 for test_num in $(seq 1 "${test_count}")
 do
   src/main tests/in_"${test_num}".txt tests/out_"${test_num}".txt result/result_"${test_num}".txt
 
-#  if [ $? -eq 137 ]; then
-#    echo "TLE" > result/result_"${test_num}".txt
-#  fi
+  exit_code=$?
+
+  echo ${exit_code}
+
+  if [[ ${exit_code} -eq 137 ]]; then
+    echo "TLE" > result/result_"${test_num}".txt
+  elif [[ ${exit_code} -eq 134 ]]; then
+    echo "MLE" > result/result_"${test_num}".txt
+  fi
+
 done
