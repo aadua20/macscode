8ea2d56ac1fcd31bcc1fc1bf5fec411ec76cc677
sskhi1 <sskhi20@freeuni.edu.ge>
2024-08-01 20:25:13 +0400

    implemented log in and sign up pages

---
+++ macscode-ui/src/components/routes/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import {Navigate} from 'react-router-dom';
+import {AuthContext} from '../../AuthContext';
+
+const PrivateRoute = ({ children }) => {
+    const { auth, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return (
+            <div className="loading-container">
+                <div className="spinner"></div>
+                <div className="loading-text">Loading...</div>
+            </div>
+        );
+    }
+
+    if (auth) {
+        return children;
+    } else {
+        return <Navigate to="/login" />;
+    }
+};
+
+export default PrivateRoute;
