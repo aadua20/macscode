0e78ab942a8598c2f62bbb8e4a2d47fdf889edbc
aadua20 <aadua20@freeuni.edu.ge>
2024-08-16 00:34:32 +0400

    add topbar to profile page

--- macscode-ui/src/components/TopBar.js
+++ macscode-ui/src/components/TopBar.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';  // Import the user profile icon
-import '../styles/TopBar.css';  // Ensure you have the corresponding CSS file for styling
-import logoImage from '../icons/M.png';  // Import the image
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
+import '../styles/TopBar.css';
+import logoImage from '../icons/M.png';
 
 
 const TopBar = () => {
     return (
         <div className="top-bar">
-            <div className="logo">
-                <img src={logoImage} alt="Logo" />
-            </div>
             <nav className="navigation">
-                <a href="/problemset">Problemset</a>
+                <a href="/" className="logo">
+                    <img src={logoImage} alt="Logo"/>
+                </a>
+            </nav>
+            <nav className="navigation">
+                <a href="/">Problemset</a>
                 <a href="/discuss">Discuss</a>
             </nav>
-            <div className="user-profile">
-                <FontAwesomeIcon icon={faUserCircle} size="2x" className="profile-icon" />
-            </div>
+            <a href="/profile" className="user-profile">
+                <FontAwesomeIcon icon={faUserCircle} size="2x" className="profile-icon"/>
+            </a>
         </div>
     );
 };
