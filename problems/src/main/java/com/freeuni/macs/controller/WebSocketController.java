bc23a11c3f8238bd252a93400c227746ff416e6d
Koba Karaputadze <kobakaraputadze701@gmail.com>
2024-08-27 16:01:15 +0400

    Finally fix!

--- problems/src/main/java/com/freeuni/macs/controller/WebSocketController.java
+++ problems/src/main/java/com/freeuni/macs/controller/WebSocketController.java
@@ -3,16 +3,12 @@ package com.freeuni.macs.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.freeuni.macs.model.SubmitRequest;
 import com.freeuni.macs.model.api.SubmitResponse;
+import com.freeuni.macs.service.JwtWebsocketService;
 import com.freeuni.macs.service.ProblemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -20,12 +16,14 @@ import java.util.List;
 @Controller
 public class WebSocketController {
 
+    private final JwtWebsocketService jwtWebsocketService;
     private final SimpMessagingTemplate messagingTemplate;
     private final ProblemService problemService;
     private final ObjectMapper objectMapper;
 
     @Autowired
-    public WebSocketController(SimpMessagingTemplate messagingTemplate, ProblemService problemService, ObjectMapper objectMapper) {
+    public WebSocketController(JwtWebsocketService jwtWebsocketService, SimpMessagingTemplate messagingTemplate, ProblemService problemService, ObjectMapper objectMapper) {
+        this.jwtWebsocketService = jwtWebsocketService;
         this.messagingTemplate = messagingTemplate;
         this.problemService = problemService;
         this.objectMapper = objectMapper;
@@ -33,9 +31,8 @@ public class WebSocketController {
 
     @MessageMapping("/submitSolution")
     public void submitSolution(String submissionJson,
-                               @Header("Authorization") String authorizationHeader,
-                               SimpMessageHeaderAccessor headerAccessor) throws Exception {
-        headerAccessor.getSessionAttributes().put("Authorization", authorizationHeader);
+                               @Header("Authorization") String authorizationHeader) throws Exception {
+        jwtWebsocketService.validateJwt(authorizationHeader);
 
         SubmitRequest submission = objectMapper.readValue(submissionJson, SubmitRequest.class);
         List<SubmitResponse> responses = problemService.submitProblem(submission);
